1094406c948f48ac313ac955ec0e08b8d5629be5
Gian Carlo <giancarlo14.11@gmail.com>
2018-01-16 20:19:27 +0100

    Modificados los metodos listFile y removeFile

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -10,7 +10,7 @@ public class MusicOrganizer
 {
     // An ArrayList for storing the file names of music files.
     private ArrayList<String> files;
-        
+
     /**
      * Create a MusicOrganizer
      */
@@ -18,7 +18,7 @@ public class MusicOrganizer
     {
         files = new ArrayList<String>();
     }
-    
+
     /**
      * Add a file to the collection.
      * @param filename The file to be added.
@@ -27,7 +27,7 @@ public class MusicOrganizer
     {
         files.add(filename);
     }
-    
+
     /**
      * Return the number of files in the collection.
      * @return The number of files in the collection.
@@ -36,30 +36,30 @@ public class MusicOrganizer
     {
         return files.size();
     }
-    
+
     /**
      * List a file from the collection.
      * @param index The index of the file to be listed.
      */
     public void listFile(int index)
     {
-        if(index >= 0 && index < files.size()) {
+        if(validIndex(index) == true) {
             String filename = files.get(index);
             System.out.println(filename);
         }
     }
-    
+
     /**
      * Remove a file from the collection.
      * @param index The index of the file to be removed.
      */
     public void removeFile(int index)
     {
-        if(index >= 0 && index < files.size()) {
+        if(validIndex(index) == true) {
             files.remove(index);
         }
     }
-    
+
     /**
      * Comprueba si se trata de un indice valido, teniendo en cuenta el tamaño
      * de la coleccion.
@@ -67,11 +67,11 @@ public class MusicOrganizer
     public void checkIndex(int checkNumber)
     {
         if(checkNumber < 0 || checkNumber > files.size() -1){
-        System.out.println("Error, el rango valido de valores es entre 0 y " + (files.size() - 1));
+            System.out.println("Error, el rango valido de valores es entre 0 y " + (files.size() - 1));
         } 
     }
-    
-        /**
+
+    /**
      * Version alternativa si se trata de un indice valido, teniendo en cuenta el tamaño
      * de la coleccion.
      */
@@ -79,7 +79,7 @@ public class MusicOrganizer
     {
         boolean checked = false;
         if(checkNumber >= 0 && checkNumber <= files.size() -1 && files.size() != 0){
-        checked = true;
+            checked = true;
         } 
         return checked;
     }
